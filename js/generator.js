8a2dcdcd54013f126fb67a0229927ba0d5552445
RisabhKedai <58504602+RisabhKedai@users.noreply.github.com>
2020-09-08 20:33:11 +0530

    final thing

---
+++ js/generator.js
@@ -0,0 +1,5 @@
+name='Ranu Mondal'
+names=['Ranu','Himesh','Neha','Selmun','Rakhi','Rakesh','Gretta','Kokila','Monisha Beta','Sunny']
+surnames=['Sawant','Dholakia','Chusia','Premi','kar-kar','Khan','Lama','Reshamia','Premdutta','Redia','Mondal','Thunenberg','Ben',"Mumru"]
+final_name=names[Math.floor(Math.random() * names.length)]+" "+surnames[Math.floor(Math.random() * surnames.length)]
+console.log(final_name)
